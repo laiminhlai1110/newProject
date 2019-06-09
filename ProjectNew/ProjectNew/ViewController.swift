16e901cce377dd1501ad106ad0a6c35210c9f822
laiminh <intern1305@snaglobal.net>
2019-06-09 15:07:07 +0700

    push2
    loading data from severNODE

--- ProjectNew/ProjectNew/ViewController.swift
+++ ProjectNew/ProjectNew/ViewController.swift
@@ -13,31 +13,75 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     @IBOutlet weak var tableView: UITableView!
     
     
-    
+    var arr:[SinhVien] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
+        
+       
+        let url = URL(string: "http://192.168.22.11:3000")
+        var req = URLRequest(url: url!)
+        req.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: url!) { (dt:Data?, ur:URLResponse?, e:Error?) in
+            
+            DispatchQueue.main.async {
+                
+                
+                do{
+                    let json = try JSONDecoder.init().decode(DULIEU.self, from: dt!)
+                    self.arr = json.sv
+                    self.tableView.reloadData()
+                    
+                    
+                }catch{
+                    print("errors")
+                }
+                
+            }
+            
+        }
+        task.resume()
+        
+        
+        
+        
     
     }
 
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return arr.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath) as! TableViewCell
+        
+        cell.lblHOTEN.text = arr[indexPath.row].HOTEN
+        cell.lblNAMSINH.text = String(arr[indexPath.row].NAMSINH)
+        
+        
+        
+        return cell
     }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    
 }
 
 
 
-
+struct  DULIEU:Codable {
+    var sv:[SinhVien]
+}
 struct SinhVien:Codable {
-    let HOTEN:String
-    let NAMSINH:Int
+    var HOTEN:String
+    var  NAMSINH:Int
     
 }
