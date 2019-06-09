0a06cc9be9d3b234e3c1456a3d551a358b17d308
laiminh <intern1305@snaglobal.net>
2019-06-09 09:05:56 +0700

    Post1

---
+++ ProjectNew/ProjectNew/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ProjectNew
+//
+//  Created by Lai Minh on 6/9/19.
+//  Copyright © 2019 LaiMinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    
+    }
+
+
+}
+
